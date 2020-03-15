b5c1ee9842db04d04baf5744ecf68adc3e632cf1
dibakash <dibakash@users.noreply.github.com>
2020-03-16 00:02:39 +0530

    Updates

---
+++ precache-manifest.6c6027ea1d446192704fbfcd7bb417ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157266e9f2f88cb9e9252204802633db",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ee2b79febb797530bafb",
+    "url": "/robofriends/static/css/main.260ded28.chunk.css"
+  },
+  {
+    "revision": "e1a452469bc71b7403ab",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.7ab315b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2b79febb797530bafb",
+    "url": "/robofriends/static/js/main.f6c46026.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  },
+  {
+    "revision": "77fa00996ecb4104c7880b8749c7c4e0",
+    "url": "/robofriends/static/media/Righteous-Regular.77fa0099.ttf"
+  }
+]);
